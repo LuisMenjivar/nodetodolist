44b979961e659258af510a220979d0906ee90817
luismenjivar <luismenjivr@gmail.com>
2015-06-05 12:46:07 -0400

    added get todos action

--- public/controllers/todoscontroller.js
+++ public/controllers/todoscontroller.js
@@ -1,5 +1,17 @@
 var todoApp = angular.module('todoApp', []);
 
-todoApp.controller('todosController', ['$scope', function($scope){
-  $scope.todos = [{name: "this is the todo name"}];
+todoApp.controller('todosController', ['$scope', '$http', function ($scope, $http){
+  $http.get('/todos').success(function(response){
+    $scope.todos = response
+  })
+
+
+ 
+  // $http.get('/persons').success(function(response){
+  //   console.log("this is comming from the /persons get");
+  // $scope.persons = response;
+  // $scope.updateButton = true
+  // // $scope.deleteButton = true
+  // });
+
 }])
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+// get todos route
+app.get('/todos', function (req, res){
+  todos = [{name: "this is the todo name"}];
+  res.json(todos);
+})
+
+
+// server
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
