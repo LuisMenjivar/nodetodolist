6d3b2a2808db0d819e202a33455d907a1627acf2
luismenjivar <luismenjivr@gmail.com>
2015-06-06 17:42:44 -0400

    changed development port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 // SERVER
 app.use(express.static(__dirname + "/public"));
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
